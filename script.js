c8abbad856cda39ae8d724bdecb3f7ea601ffa9e
Gabriel Fellipe <gfellipe84@gmail.com>
2025-04-16 21:19:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+let display = document.getElementById("display");
+function adicionarValor(valor) {
+    display.value += valor;
+}
+function calcular() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Erro";
+    }
+}
